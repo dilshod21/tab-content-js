fdc0c7552c71959116c9f9b52278de1e7fa545ff
dilshod21 <dilshodmahmudov2003@gmail.com>
2023-04-19 12:57:21 +0500

    added js file and js with codes for tab project

---
+++ tab.js
@@ -0,0 +1,34 @@
+let parentBtns = document.querySelector(".btn-box");
+let btns = document.querySelectorAll(".btn");
+let contentBoxes = document.querySelectorAll(".content");
+
+
+function hideContent(){
+    contentBoxes.forEach((item) => {
+        item.classList.remove("active");
+    });
+    btns.forEach((item) => {
+        item.classList.remove('active');
+    })
+};
+
+function showContent(i = 0){
+    contentBoxes[i].classList.add("active");
+    btns[i].classList.add("active");
+};
+
+
+
+
+parentBtns.addEventListener("click", (event) => {
+    let targetOne = event.target;
+
+    if(targetOne && targetOne.classList.contains("btn")){
+        btns.forEach((item, index) => {
+            if(targetOne == item){
+                hideContent();
+                showContent(index);
+            }
+        });
+    }
+});
